328cfef1da10ac3adad62b873c75448066ec51c7
TheLoneKing <dkumar.kovai@gmail.com>
2019-08-07 06:40:23 +0530

    Rewrite Binary Search Tree implementation without underlying list

--- src/main/kotlin/org/theloneking/algorithms/BinarySearchTree.kt
+++ src/main/kotlin/org/theloneking/algorithms/BinarySearchTree.kt
@@ -1,24 +1,22 @@
 package org.theloneking.algorithms
 
 class BinarySearchTree {
-    val tree: MutableList<BinarySearchTreeNode> = mutableListOf()
+    private var rootNode: BinarySearchTreeNode? = null
 
-    fun insert(element: Int): Boolean {
-        return when (tree.size) {
-            0 -> tree.add(BinarySearchTreeNode(element))
-            else -> findPositionAndInsert(element, 0)
-        }
+    fun insert(element: Int) {
+        rootNode = insertRecursive(element, rootNode)
     }
 
-    fun find(element: Int): Int {
-        return when {
-            (tree.size > 0) -> findPosition(element, 0)
-            else -> -1
-        }
+    fun containsNode(element: Int): Boolean {
+        return containsNodeRecursive(element, rootNode)
     }
 
+    /**
+     * Rank is the number of nodes in the tree whose values are either less than or equal to a given value.
+     * This method will return the rank for a given value.
+     */
     fun rank(element: Int): Int {
-        return calculateRank(element, 0)
+        return calculateRank(element, rootNode)
     }
 
     // TODO
@@ -26,69 +24,53 @@ class BinarySearchTree {
         return false
     }
 
-    private fun findPositionAndInsert(element: Int, currentIndex: Int): Boolean {
-        if (element >= tree[currentIndex].data) {
-            return if (tree[currentIndex].rightPointer == null) {
-                val newNode = BinarySearchTreeNode(element)
-                newNode.parentPointer = currentIndex
-                tree.add(newNode)
-                tree[currentIndex].rightPointer = tree.size - 1
-                tree[currentIndex].subTreeSize++
-                true
-            } else {
-                val inserted = findPositionAndInsert(element, tree[currentIndex].rightPointer ?: tree.size)
-                if (inserted) {
-                    tree[currentIndex].subTreeSize++
-                }
-                inserted
+    private fun insertRecursive(element: Int, currentNode: BinarySearchTreeNode?): BinarySearchTreeNode {
+        if (currentNode == null) {
+            return BinarySearchTreeNode(element)
+        }
+
+        when {
+            element < currentNode.value -> currentNode.leftNode = insertRecursive(element, currentNode.leftNode)
+            element > currentNode.value -> currentNode.rightNode = insertRecursive(element, currentNode.rightNode)
+            else -> return currentNode // element == currentNode.value (element is already present in the tree)
+        }
+
+        // Recalculate subtree sizes
+        currentNode.subTreeSize = 1 + (currentNode.leftNode?.subTreeSize ?: 0) + (currentNode.rightNode?.subTreeSize ?: 0)
+        return currentNode
+    }
+
+    private fun containsNodeRecursive(element: Int, currentNode: BinarySearchTreeNode?): Boolean {
+        return if (currentNode != null) {
+            when {
+                element < currentNode.value -> containsNodeRecursive(element, currentNode.leftNode)
+                element > currentNode.value -> containsNodeRecursive(element, currentNode.rightNode)
+                else -> true // element == currentNode.value
             }
         } else {
-            return if (tree[currentIndex].leftPointer == null) {
-                val newNode = BinarySearchTreeNode(element)
-                newNode.parentPointer = currentIndex
-                tree.add(newNode)
-                tree[currentIndex].leftPointer = tree.size - 1
-                tree[currentIndex].subTreeSize++
-                true
-            } else {
-                val inserted = findPositionAndInsert(element, tree[currentIndex].leftPointer ?: tree.size)
-                if (inserted) {
-                    tree[currentIndex].subTreeSize++
-                }
-                inserted
-            }
+            false
         }
     }
 
-    private fun findPosition(element: Int, currentIndex: Int?): Int {
-        return when {
-            (currentIndex != null && element == tree[currentIndex].data) -> currentIndex
-            (currentIndex != null && element > tree[currentIndex].data) -> findPosition(element, tree[currentIndex].rightPointer)
-            (currentIndex != null && element < tree[currentIndex].data) -> findPosition(element, tree[currentIndex].leftPointer)
-            else -> -1
-        }
-    }
-
-    private fun calculateRank(element: Int, index: Int): Int {
+    private fun calculateRank(element: Int, currentNode: BinarySearchTreeNode?): Int {
         var rank = 0
-        val leftPtr = tree[index].leftPointer
-        val rightPtr = tree[index].rightPointer
-        if (tree[index].data <= element) {
-            rank++
-            rank = if (leftPtr != null) rank + tree[leftPtr].subTreeSize else rank
-            if (rightPtr != null) {
-                rank += calculateRank(element, rightPtr)
+
+        if (currentNode != null) {
+            if (currentNode.value <= element) {
+                rank++ // for the current node
+                rank += currentNode.leftNode?.subTreeSize ?: 0 // all nodes in left subtree are < element
+                rank += calculateRank(element, currentNode.rightNode) // traverse right subtree & see if any node < element
+            } else {
+                rank += calculateRank(element, currentNode.leftNode) // traverse through left subtree & see if any node < element
             }
-        } else if (leftPtr != null) {
-            rank += calculateRank(element, leftPtr)
         }
+
         return rank
     }
 
-    inner class BinarySearchTreeNode(val data: Int) {
-        var parentPointer: Int? = null
-        var rightPointer: Int? = null
-        var leftPointer: Int? = null
+    inner class BinarySearchTreeNode(val value: Int) {
+        var rightNode: BinarySearchTreeNode? = null
+        var leftNode: BinarySearchTreeNode? = null
         var subTreeSize: Int = 1
     }
 }
--- src/test/kotlin/org/theloneking/algorithms/BinarySearchTreeTest.kt
+++ src/test/kotlin/org/theloneking/algorithms/BinarySearchTreeTest.kt
@@ -2,48 +2,34 @@ package org.theloneking.algorithms
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 class BinarySearchTreeTest: BaseTest() {
     @Test
-    fun testInsertAndFind() {
+    fun testInsertAndContainsNode() {
         val testArray: Array<Int> = arrayOf(32, 43, 11, 9, 4, 55, 99, 1, 19, 10)
         val binarySearchTree = BinarySearchTree()
         testArray.forEach { binarySearchTree.insert(it) }
 
-        val indexOfTen = binarySearchTree.find(10)
-        assertNull(binarySearchTree.tree[indexOfTen].leftPointer)
-        assertNull(binarySearchTree.tree[indexOfTen].rightPointer)
-        assertEquals(3, binarySearchTree.tree[indexOfTen].parentPointer)
+        assertTrue { binarySearchTree.containsNode(32) }
 
-        val indexOfNineteen = binarySearchTree.find(19)
-        assertNull(binarySearchTree.tree[indexOfNineteen].leftPointer)
-        assertNull(binarySearchTree.tree[indexOfNineteen].rightPointer)
-        assertEquals(2, binarySearchTree.tree[indexOfNineteen].parentPointer)
+        assertTrue { binarySearchTree.containsNode(43) }
 
-        val indexOfNine = binarySearchTree.find(9)
-        assertEquals(4, binarySearchTree.tree[indexOfNine].leftPointer)
-        assertEquals(9, binarySearchTree.tree[indexOfNine].rightPointer)
-        assertEquals(2, binarySearchTree.tree[indexOfNine].parentPointer)
-    }
+        assertTrue { binarySearchTree.containsNode(11) }
 
-    @Test
-    fun testSubTreeSize() {
-        val testArray: Array<Int> = arrayOf(32, 43, 11, 9, 4, 55, 99, 1, 19, 10)
-        val binarySearchTree = BinarySearchTree()
-        testArray.forEach { binarySearchTree.insert(it) }
+        assertTrue { binarySearchTree.containsNode(9) }
 
-        val indexOfTen = binarySearchTree.find(10)
-        assertEquals(1, binarySearchTree.tree[indexOfTen].subTreeSize)
+        assertTrue { binarySearchTree.containsNode(4) }
 
-        val indexOfEleven = binarySearchTree.find(11)
-        assertEquals(6, binarySearchTree.tree[indexOfEleven].subTreeSize)
+        assertTrue { binarySearchTree.containsNode(55) }
 
-        val indexOfFortyThree = binarySearchTree.find(43)
-        assertEquals(3, binarySearchTree.tree[indexOfFortyThree].subTreeSize)
+        assertTrue { binarySearchTree.containsNode(99) }
 
-        val indexOfThirtyTwo = binarySearchTree.find(32)
-        assertEquals(10, binarySearchTree.tree[indexOfThirtyTwo].subTreeSize)
+        assertTrue { binarySearchTree.containsNode(1) }
+
+        assertTrue { binarySearchTree.containsNode(19) }
+
+        assertTrue { binarySearchTree.containsNode(10) }
     }
 
     @Test
@@ -52,16 +38,14 @@ class BinarySearchTreeTest: BaseTest() {
         val binarySearchTree = BinarySearchTree()
         testArray.forEach { binarySearchTree.insert(it) }
 
-        val rankOfNineteen = binarySearchTree.rank(19)
-        assertEquals(6, rankOfNineteen)
+        assertEquals(6, binarySearchTree.rank(19))
 
-        val rankOfFiftyFive = binarySearchTree.rank(55)
-        assertEquals(9, rankOfFiftyFive)
+        assertEquals(9, binarySearchTree.rank(55))
 
-        val rankOfThirtyTwo = binarySearchTree.rank(32)
-        assertEquals(7, rankOfThirtyTwo)
+        assertEquals(7, binarySearchTree.rank(32))
 
-        val rankOfOne = binarySearchTree.rank(1)
-        assertEquals(1, rankOfOne)
+        assertEquals(1, binarySearchTree.rank(1))
+
+        assertEquals(0, binarySearchTree.rank(0))
     }
 }
